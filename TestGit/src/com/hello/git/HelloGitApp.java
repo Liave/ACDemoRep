2aeccafde062887b8663aa0a8599723b2e5ac4fa
Liav Elias <liav.elias@audiocodes.com>
2014-07-06 14:12:32 +0300

    initial commit

---
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -0,0 +1,10 @@
+package com.hello.git;
+
+public class HelloGitApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from git version 1");
+	}
+
+}
