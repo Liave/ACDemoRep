d532c22e695c976163daffb59e983b701d8534e5
Liav Elias <liav.elias@audiocodes.com>
2014-07-06 14:15:07 +0300

    aaa

--- TestGit/src/com/hello/git/HelloGitApp.java
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -5,6 +5,7 @@ public class HelloGitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from git version 1");
+		System.out.println("Hello from git version 2");
 	}
 
 }
