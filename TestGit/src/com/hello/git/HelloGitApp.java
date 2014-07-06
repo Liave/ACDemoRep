5c76542415867640235254b6016c9048a44ac8ac
Liav Elias <liav.elias@audiocodes.com>
2014-07-06 17:31:47 +0300

    1

--- TestGit/src/com/hello/git/HelloGitApp.java
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -3,16 +3,7 @@ package com.hello.git;
 public class HelloGitApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello from git version 1");
-		System.out.println("Hello from git version 2");
-		System.out.println("Hello from git version 3");
-		System.out.println("Hello from git version 4");
-		System.out.println("Hello from git version 5");
-		System.out.println("Hello from git version 6");
-		System.out.println("Hello from git version 7");
-		System.out.println("Hello from git version 8");
-
 	}
 
 }
