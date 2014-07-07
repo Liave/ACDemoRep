d3f2aac33fa39426c409c011e05c2d8b90953dfa
Liav Elias <liav.elias@audiocodes.com>
2014-07-07 10:46:49 +0300

    dfff

--- TestGit/src/com/hello/git/HelloGitApp.java
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -6,6 +6,7 @@ public class HelloGitApp {
 		System.out.println("Hello from git version 1");
 		System.out.println("Hello from git version 2");
 		System.out.println("Hello from git version 3");
+		System.out.println("Hello from git version 5");
 	}
 
 }
