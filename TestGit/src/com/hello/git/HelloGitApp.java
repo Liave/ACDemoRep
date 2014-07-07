5cc04ccbb040954a43c1897472cf447fe68aab12
Liav Elias <liav.elias@audiocodes.com>
2014-07-07 08:41:13 +0300

    remove all messages

--- TestGit/src/com/hello/git/HelloGitApp.java
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -4,17 +4,6 @@ public class HelloGitApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from git version 1");
-		System.out.println("Hello from git version 2");
-		System.out.println("Hello from git version 3");
-		System.out.println("Hello from git version 4");
-		System.out.println("Hello from git version 5");
-		System.out.println("Hello from git version 6");
-		System.out.println("Hello from git version 7");
-		System.out.println("Hello from git version 8");
-		System.out.println("Hello from git version 9");
-		System.out.println("Hello from git version 10");
-		System.out.println("Hello from git version 11");
-		System.out.println("Hello from git version 12");
 	}
 
 }
