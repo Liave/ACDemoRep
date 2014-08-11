e621ce2e7c2ba7c6d263d19be89eace6fe3d008e
Home <Home@Home-PC>
2014-08-11 21:32:15 +0300

    fff

--- TestGit/src/com/hello/git/HelloGitApp.java
+++ TestGit/src/com/hello/git/HelloGitApp.java
@@ -8,13 +8,7 @@ public class HelloGitApp {
 		System.out.println("Hello from git version 3");
 		System.out.println("Hello from git version 4");
 		System.out.println("Hello from git version 5");
-		System.out.println("Hello from git version 6");
-		System.out.println("Hello from git version 7");
-		System.out.println("Hello from git version 8");
-		System.out.println("Hello from git version 9");
-		System.out.println("Hello from git version 10");
-		System.out.println("Hello from git version 11");
-		System.out.println("Hello from git version 12");
 	}
 
 }
+ 
